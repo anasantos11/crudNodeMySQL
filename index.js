c455fb4b9016a8ee15c8db9bd725dfa28ae7806b
Ana <anasantos1103@gmail.com>
2018-10-02 19:55:14 -0300

    Criado conexão para servidor na porta 3000 e no banco de dados

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log('Servidor local rodando na porta 3000!');
+});
+
+const database = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "tomcat",
+  database: "nodejs"
+});
+
+database.connect(function (error) {
+  if (error) {
+    console.error("Erro:  " + error.message);
+  }
+  console.info("Conectado com o banco de dados.")
+});
+
+app.get('/', function (req, res) {
+  res.send('Servidor local rodando na porta 3000!');
+});
