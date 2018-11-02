b6b94e4f29ab67b3ce93d0a6c4b9574a6cfd5054
Ana <anasantos1103@gmail.com>
2018-11-02 13:02:40 -0300

    Ajustado conexão com o mssql

--- src/config/data.global.js
+++ src/config/data.global.js
@@ -1,14 +1,28 @@
-var sql = require("mssql");
-const isLocal = !process.env.SQLAZURECONNSTR_ConnectionString;
-const config = isLocal ? require('../../data-sql.local') : process.env.SQLAZURECONNSTR_ConnectionString;
+var sql = require('mssql');
+const isLocal = !JSON.stringify(process.env.SQLAZURECONNSTR_ConnectionString);
+
+function getConfig(){
+    const connectionString = process.env.SQLAZURECONNSTR_ConnectionString.split(';');
+    const config = {
+        server: connectionString[0].split('=')[1].split(':')[1].split(',')[0],
+        database: connectionString[1].split('=')[1],
+        user: connectionString[2].split('=')[1],
+        password: connectionString[3].split('=')[1],
+        options: {
+            encrypt: true
+        }
+    };
+   return config;
+};
 
 module.exports = {
     sqlServer: function () {
+        const config = isLocal ? require('../data-sql.local') : getConfig();
         sql.connect(config, function (err) {
             if (err) {
-                console.log(err)
+                console.log('Erro: ' + err);
             } else {
-                console.log("Database connected");
+                console.log('Database connected');
             }
         });
     }
